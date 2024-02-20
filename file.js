9c9d04bb68672d6f0ef0809cadbbd4b7cbe7bc11
Sagar Khadka <khadkasagar109@gmail.com>
2024-02-20 22:45:09 +0545

    http server created

---
+++ file.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+// ? sync
+// read file
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+console.log(textIn)
+
+// write file
+const textOut = `This is what we know about teh avocado: ${textIn}. \nCreated on ${Date.now()}`
+
+fs.writeFileSync('./txt/output.txt', textOut)
+
+// * async
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2)
+    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+      console.log(data3)
+
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        console.log('Files written 👌')
+      })
+    })
+  })
+})
+console.log('Reading file...')
--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-let fs = require('fs')
+const http = require('http')
 
-// ? sync
-// read file
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
-// console.log(textIn)
-
-// write file
-// const textOut = `This is what we know about teh avocado: ${textIn}. \nCreated on ${Date.now()}`
-
-// fs.writeFileSync('./txt/output.txt', textOut)
-
-// * async
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-  console.log(data)
+const server = http.createServer((req, res) => {
+  console.log(req)
+  res.end('Hello from the server')
 })
-console.log('Reading file...')
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening in 8000')
+})
